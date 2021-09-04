31f439e1cf41b677821cfa854cc1c339547291ac
Hessam Hashemi <hessam.h.ir@gmail.com>
2021-09-04 23:00:44 +0430

    add parallel test and refactor code for reflect all changes in code

--- queue_test.go
+++ queue_test.go
@@ -7,12 +7,12 @@ import (
 
 func Test_Enqueue(t *testing.T) {
 	q := NewQueue()
-	q.Enqueue(&Node{data: "hello world"})
-	q.Enqueue(&Node{data: "my name is hessam"})
-	q.Enqueue(&Node{data: 12})
-	q.Enqueue(&Node{data: "hello world"})
-	q.Enqueue(&Node{data: "my name is hessam"})
-	q.Enqueue(&Node{data: 12})
+	q.Enqueue("hello world")
+	q.Enqueue("my name is hessam")
+	q.Enqueue(12)
+	q.Enqueue("hello world")
+	q.Enqueue("my name is hessam")
+	q.Enqueue(12)
 
 	l := q.Len()
 	log.Printf("queue length: %d", l)
@@ -32,8 +32,35 @@ func BenchmarkEnqueue(b *testing.B) {
 	b.ReportAllocs()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		q.Enqueue(&Node{data: "hello world"})
-		q.Enqueue(&Node{data: []byte("my name is hessam")})
-		q.Enqueue(&Node{data: 12})
+		q.Enqueue("hello world")
 	}
+}
+
+func BenchmarkEnqueue_Parallel(b *testing.B) {
+	q := NewQueue()
+	b.ReportAllocs()
+	b.SetParallelism(1000)
+	b.ResetTimer()
+	//for i:=0 ;i<b.N; i++{
+	b.Run("enqueue", func(b *testing.B) {
+		b.ReportAllocs()
+		b.SetParallelism(1000)
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				q.Enqueue("hello world")
+			}
+		})
+	})
+	b.Run("dequeue", func(b *testing.B) {
+		b.ReportAllocs()
+		b.SetParallelism(1000)
+		b.ResetTimer()
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				_, _ = q.Dequeue()
+			}
+		})
+	})
+	//}
 }
