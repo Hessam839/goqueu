3ed8aa7070e784ca2821f17c92d6c8f5d7e00e69
Hessam Hashemi <hessam.h.ir@gmail.com>
2021-09-04 23:00:09 +0430

    replace all lock functions and replace with CAS method. change enqueue parameter from node to interface

--- goqueu.go
+++ goqueu.go
@@ -3,6 +3,7 @@ package goqueu
 import (
 	"errors"
 	"sync"
+	"sync/atomic"
 )
 
 func NewQueue() *Queue {
@@ -14,43 +15,57 @@ func NewQueue() *Queue {
 	}
 }
 
-func (q *Queue) Enqueue(node *Node) {
-	q.lock.Lock()
-	defer q.lock.Unlock()
+func (q *Queue) Enqueue(data interface{}) {
+	//q.lock.Lock()
+	//defer q.lock.Unlock()
+	node := &Node{data: data}
+	var check int64 = 0
+	if atomic.CompareAndSwapInt64(&check, 0, 1) {
+		defer atomic.StoreInt64(&check, 0)
+	} else {
+		if q.head == nil {
+			q.head = node
+			//q.head.ptr = nil
+			q.tail = node
+			//q.tail.ptr = nil
+			q.len++
+			return
+		}
 
-	if q.head == nil {
-		q.head = node
-		//q.head.ptr = nil
+		q.tail.ptr = node
 		q.tail = node
-		//q.tail.ptr = nil
 		q.len++
-		return
 	}
-
-	q.tail.ptr = node
-	q.tail = node
-	q.len++
-	return
 }
 
 func (q *Queue) Dequeue() (interface{}, error) {
-	q.lock.Lock()
-	defer q.lock.Unlock()
+	//q.lock.Lock()
+	//defer q.lock.Unlock()
+	var check int64 = 0
+	if atomic.CompareAndSwapInt64(&check, 0, 1) {
+		defer atomic.StoreInt64(&check, 0)
+	} else {
+		if q.head == nil {
+			return nil, errors.New("queue is empty")
+		}
 
-	if q.head == nil {
-		return nil, errors.New("queue is empty")
+		node := q.head
+		q.head = q.head.ptr
+		q.len--
+		node.ptr = nil
+		return node.data, nil
 	}
-
-	node := q.head
-	q.head = q.head.ptr
-	q.len--
-	node.ptr = nil
-	return node.data, nil
+	return nil, nil
 }
 
 func (q *Queue) Len() int32 {
-	q.lock.Lock()
-	defer q.lock.Unlock()
-
-	return q.len
+	//q.lock.Lock()
+	//defer q.lock.Unlock()
+	var check int64 = 0
+	if atomic.CompareAndSwapInt64(&check, 0, 1) {
+		defer atomic.StoreInt64(&check, 0)
+	} else {
+		return q.len
+	}
+	return 0
 }
